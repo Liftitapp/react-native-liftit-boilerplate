22dfdd38c336f011f98bbf89eedbe6d8aa5088fc
Santiago Quinto <santiago@liftit.co>
2017-06-22 16:58:39 -0500

    Commit to check the Codeclimate user recognition

--- app/components/launch_panel/launch_panel.js
+++ app/components/launch_panel/launch_panel.js
@@ -41,8 +41,8 @@ const launchPanel = ({
 
 const alertMessage = () => {
   Alert.alert(
-    'Alert Title',
-    'My Alert Msg',
+    'Title',
+    'Alert Msg',
     [
       { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
       { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
