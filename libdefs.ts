7df8cd3e8dad84933017549df82032d6d39e0c58
Santiago Quinto <santiago@liftit.co>
2017-06-27 17:05:11 -0500

    flow package added

---
+++ libdefs.ts
@@ -0,0 +1,9 @@
+declare module 'react-native' {
+  declare var exports: any;
+}
+declare module 'react-native-i18n' {
+  declare var exports: any;
+}
+declare module 'react-native-config' {
+  declare var exports: any;
+}
