e8410d4fe7e8c07ec099b9c0c81d5994e56eff02
Santiago Quinto <santiago@liftit.co>
2017-07-05 14:49:41 -0500

    Frontend team annotation's done

--- android/app/src/main/java/com/reactnativeliftitboilerplate/MainApplication.java
+++ android/app/src/main/java/com/reactnativeliftitboilerplate/MainApplication.java
@@ -26,9 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNSentryPackage(MainApplication.this),
-            new ReactNativeConfigPackage(),
-              new ReactNativeI18n()
+          new RNSentryPackage(MainApplication.this),
+          new ReactNativeConfigPackage(),
+          new ReactNativeI18n()
       );
     }
   };
